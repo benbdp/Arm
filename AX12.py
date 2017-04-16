90f36ede89ae3d28d4760f9b272bd5744718e091
Benjamin Plamondon <benjamin.plamondon@gmail.com>
2017-04-16 02:46:32 -0400

    testing on raspberry pi

--- AX12.py
+++ AX12.py
@@ -2,31 +2,31 @@ import numpy
 import Pynamixel
 
 
-# hardware = Pynamixel.hardwares.USB2AX("/dev/ttyACM0", 1000000)
-# system = Pynamixel.System(Pynamixel.Bus(hardware))
-# servo = system.add_device(Pynamixel.devices.AX12, 1)
-# servo.goal_position.write(0x200)
-# servo.moving.read()
+hardware = Pynamixel.hardwares.USB2AX("/dev/ttyACM0", 1000000)
+system = Pynamixel.System(Pynamixel.Bus(hardware))
+servo = system.add_device(Pynamixel.devices.AX12, 1)
+servo.goal_position.write(0x200)
 
 
-degree = 150
 
-
-def deg2hex(degree):
-    max_lim = 1023
-    min_lim = 0
-    decimal = int((256 * degree) / 75)
-    print(decimal)
-    if decimal > max_lim:
-        pass
-    elif decimal < min_lim:
-        pass
-    else:
-        pos = hex(decimal)
-        print(pos)
-
-
-deg2hex(degree)
+# degree = 150
+#
+#
+# def deg2hex(degree):
+#     max_lim = 1023
+#     min_lim = 0
+#     decimal = int((256 * degree) / 75)
+#     print(decimal)
+#     if decimal > max_lim:
+#         pass
+#     elif decimal < min_lim:
+#         pass
+#     else:
+#         pos = hex(decimal)
+#         print(pos)
+#
+#
+# deg2hex(degree)
 
 
 
