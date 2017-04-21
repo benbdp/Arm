eb63e02fde4b0375255060729c05bd896d41f41d
wjd14 <will@ayerdoyles.org>
2017-04-21 19:46:23 -0400

    little test script for manual control of the arm

--- controlTheArm.py
+++ controlTheArm.py
@@ -7,37 +7,96 @@ Created on Wed Apr 19 19:37:40 2017
 """
 
 import numpy as np
+from pyax12.conection import Connection
 
-import Pynamixel
+sc = Connection(port = '/dev/ttyACM0', baudrate = 1000000)
 
-hardware = Pynamixel.hardwares.USB2AX("/dev/ttyACM0", 1000000)
-system = Pynamixel.System(Pynamixel.Bus(hardware))
-servo1 = system.add_device(Pynamixel.devices.AX12, 1)
-servo2 = system.add_device(Pynamixel.devices.AX12, 2)
-servo3 = system.add_device(Pynamixel.devices.AX12, 3)
 
-position = servo1.present_position;
-servo1.moving_speed.write(0x3ff);
-servo2.moving_speed.write(0x3ff);
-servo3.moving_speed.write(0x3ff);
-working = True;
-#position = 0
-while(working):
+def read_single_keypress():
+    """Waits for a single keypress on stdin.
+
+    This is a silly function to call if you need to do it a lot because it has
+    to store stdin's current setup, setup stdin for reading single keystrokes
+    then read the single keystroke then revert stdin back after reading the
+    keystroke.
+
+    Returns the character of the key that was pressed (zero on
+    KeyboardInterrupt which can happen when a signal gets handled)
+
+    """
+    import termios, fcntl, sys, os
+    fd = sys.stdin.fileno()
+    # save old state
+    flags_save = fcntl.fcntl(fd, fcntl.F_GETFL)
+    attrs_save = termios.tcgetattr(fd)
+    # make raw - the way to do this comes from the termios(3) man page.
+    attrs = list(attrs_save) # copy the stored version to update
+    # iflag
+    attrs[0] &= ~(termios.IGNBRK | termios.BRKINT | termios.PARMRK 
+                  | termios.ISTRIP | termios.INLCR | termios. IGNCR 
+                  | termios.ICRNL | termios.IXON )
+    # oflag
+    attrs[1] &= ~termios.OPOST
+    # cflag
+    attrs[2] &= ~(termios.CSIZE | termios. PARENB)
+    attrs[2] |= termios.CS8
+    # lflag
+    attrs[3] &= ~(termios.ECHONL | termios.ECHO | termios.ICANON
+                  | termios.ISIG | termios.IEXTEN)
+    termios.tcsetattr(fd, termios.TCSANOW, attrs)
+    # turn off non-blocking
+    fcntl.fcntl(fd, fcntl.F_SETFL, flags_save & ~os.O_NONBLOCK)
+    # read a single keystroke
+    try:
+        ret = sys.stdin.read(1) # returns a single character
+    except KeyboardInterrupt: 
+        ret = 0
+    finally:
+        # restore old state
+        termios.tcsetattr(fd, termios.TCSAFLUSH, attrs_save)
+        fcntl.fcntl(fd, fcntl.F_SETFL, flags_save)
+    return ret
+
+def moveAnyServo(num,pos):
+    sc.goto(num, pos, 50, True)
     
-    direction = raw_input('Enter Direction, Up is w, Down is s, stop is q, quit is anything else:')
-    print(direction)
-    
-    if(direction == "w"):
-        servo1.goal_position.write(0x3ff)
-    elif(direction == "s"):
-        servo1.goal_position.write(0x000)
-    elif(direction == "q"):
-        servo1.goal_position.write(servo1.present_position.read())
-    else:
-        working = False;
+
+
+
+
+if __name__ == "__main__":
+    servo_num = 2
+    while True:
+        key = read_single_keypress()
+        if key == 'q':
+            break
+        if key == 'w':
+            moveAnyServo(servo_num, sc.get_present_position(servo_num, True) + 1)
+        if key == 's':
+            moveAnyServo(servo_num, sc.get_present_position(servo_num, True) - 1)
+        if key == '2':
+            servo_num = 2;
+        if key == '3':
+            servo_num = 3;
+        if key == '4':
+            servo_num = 4;
         
         
         
         
-print('Done running the arm')
-    
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
