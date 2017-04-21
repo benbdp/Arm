30c78c6e1269c114eddfd9fc0bd918330b9c95a4
Benjamin Plamondon <benjamin.plamondon@gmail.com>
2017-04-20 22:30:18 -0400

    testing new library on pi

--- LinuxAX12.py
+++ LinuxAX12.py
@@ -2,7 +2,7 @@ from pyax12.connection import Connection
 import time
 sc = Connection(port="/dev/ttyACM0", baudrate=1000000)
 id = 1
-sc.goto(id, 0, speed=512, degrees=True)
-time.sleep(1)
-sc.goto(id, -45, speed=512, degrees=True)
+sc.goto(id, 10, speed=512, degrees=True)
+# time.sleep(1)
+# sc.goto(id, -45, speed=512, degrees=True)
 sc.close()
