007372eda284182d83a28c954b9406b5e43175eb
Benjamin Plamondon <benjamin.plamondon@gmail.com>
2017-04-21 19:44:17 -0400

    calculate the current position of end effector

--- Control.py
+++ Control.py
@@ -58,7 +58,7 @@ def servo4pos():
 
 
 def endx(leg1,joint1,leg2,joint2,leg3,joint3):
-    x = (leg1*np.cos(joint1))+(leg2*np.cos(joint1+joint2))+(leg3*np.cos(joint1+joint2+joint3))
+    x = (leg1*np.cos(joint1))#+(leg2*np.cos(joint1+joint2))+(leg3*np.cos(joint1+joint2+joint3))
     return(x)
 
 def endy(leg1,joint1,leg2,joint2,leg3,joint3):
