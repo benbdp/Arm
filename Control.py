3f818c69e8456921a71257ed5042f9f300ac8943
Benjamin Plamondon <benjamin.plamondon@gmail.com>
2017-04-21 14:45:19 -0400

    Adding logic to prevent new entry if servo is still moving

--- Control.py
+++ Control.py
@@ -3,7 +3,6 @@ import time
 
 sc = Connection(port='/dev/ttyACM0', baudrate=1000000)
 
-
 def servo2(pos):
     if angle < 0:
         print("illegal pos")
@@ -12,7 +11,6 @@ def servo2(pos):
         sc.goto(2, pos, 100, True)
     elif pos == 0:
         sc.goto(2, pos, 100, True)
-
 
 def servo3(pos):
     sc.goto(3, pos, 100, True)
@@ -24,13 +22,12 @@ def servo4(pos):
     elif pos > 0:
         pos = -pos
         sc.goto(4, pos, 100, True)
-
     elif pos == 0:
         sc.goto(4, pos, 100, True)
 
 try:
     working = False
-    if (sc.ping(2) == True):
+    if (sc.ping(2) == True): # test the connection
         working = True
         print("working")
 
@@ -49,6 +46,7 @@ try:
         elif servonum == 4:
             servo4(angle)
 
+        print(sc.is_moving(servonum))
         time.sleep(1)
 
 except:
