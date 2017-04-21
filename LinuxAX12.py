b82674783c5a61e0f1626164e1c204c4eacdd7e7
Benjamin Plamondon <benjamin.plamondon@gmail.com>
2017-04-20 22:28:07 -0400

    testing new library on pi

--- LinuxAX12.py
+++ LinuxAX12.py
@@ -1,7 +1,11 @@
 from pyax12.connection import Connection
 
 sc = Connection(port="/dev/ttyACM0", baudrate=1000000)
-ids = sc.scan()
-for id in ids:
-    print(id)
+# ids = sc.scan()
+# for id in ids:
+#     print(id)
+# sc.close()
+
+id = 1
+sc.pretty_print_control_table(id)
 sc.close()
