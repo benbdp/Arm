8304a579353507337266e58b2d6e3bb17e74a7a5
Benjamin Plamondon <benjamin.plamondon@gmail.com>
2017-04-22 00:08:34 -0400

    testing camera on pi

--- pivision.py
+++ pivision.py
@@ -32,7 +32,7 @@ im2, contours, hierarchy = cv2.findContours(erode, cv2.RETR_TREE, cv2.CHAIN_APPR
 newcontours = []
 for cnt in contours:
     area = cv2.contourArea(cnt)
-    if area > 2:
+    if area > 200:
         newcontours.append(cnt)
         area = cv2.contourArea(cnt)
         print(area)
