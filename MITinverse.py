2f37d6d5b02f69001320d002e6d588f153e75a70
Benjamin Plamondon <benjamin.plamondon@gmail.com>
2017-04-22 16:59:31 -0400

    testing camera on pi

--- MITinverse.py
+++ MITinverse.py
@@ -14,14 +14,9 @@ tx = 25.737
 
 ty = 42.221
 
-# convert degrees to rad
-
-eo = np.deg2rad(eo)
-
-print(eo)
-
 # find wrist positions
 def wristposx(tx,leg3,eo):
+    eo = np.deg2rad(eo)
     if eo < (np.pi/2):
         xw = tx-leg3*(np.cos(eo))
         return xw
@@ -32,6 +27,7 @@ def wristposx(tx,leg3,eo):
 print("xw: ",wristposx(tx,leg3,eo))
 
 def wristposy(ty,leg3,eo):
+    eo = np.deg2rad(eo)
     yw = ty-leg3*(np.sin(eo))
     return yw
 
@@ -63,6 +59,7 @@ def joint2(xw,yw,leg1,leg2):
 
 #theta three
 def joint3(xw,yw,leg1,leg2,eo):
+    eo = np.deg2rad(eo)
     t3 = eo - joint1(xw,yw,leg1,leg2) - joint2(xw,yw,leg1,leg2)  # add logic to add or subtract joint 2 depending on situation
     return t3
 
@@ -73,9 +70,9 @@ def joint3(xw,yw,leg1,leg2,eo):
 
 
 
-def inversek():
+def inversek(tx,ty,leg1, leg2, leg3, eo):
     t1 = np.degrees(joint1(wristposx(tx, leg3, eo), wristposy(ty, leg3, eo), leg1, leg2))
     t2 = np.degrees(joint2(wristposx(tx, leg3, eo), wristposy(ty, leg3, eo), leg1, leg2))
     t3 = np.degrees(joint3(wristposx(tx, leg3, eo), wristposy(ty, leg3, eo), leg1, leg2, eo))
 
-    return t1,t2,t3
+    return t1, t2, t3
