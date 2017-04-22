435657249640b9f7290fef30de9b2c94c98bbcca
Benjamin Plamondon <benjamin.plamondon@gmail.com>
2017-04-22 00:13:23 -0400

    testing camera on pi

--- pivision.py
+++ pivision.py
@@ -44,8 +44,10 @@ print("center: ",center,"radius: ",radius)
 area = 3.14 * (radius ** 2)
 print("area: ", area)
 
+cv2.circle(image,int(center),int(radius),(0, 255, 0), 3)
+
 cv2.imshow("erode",erode)
 # cv2.imshow("mask",mask)
 # cv2.imshow("hsv",hsv)
-# cv2.imshow("Image", image)
+cv2.imshow("Image", image)
 cv2.waitKey(0)
