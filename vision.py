b761b85de3ad0dd50197d519ab369c438474c5fc
Benjamin Plamondon <benjamin.plamondon@gmail.com>
2017-04-23 19:24:38 -0400

    playing with cascades

--- vision.py
+++ vision.py
@@ -21,28 +21,32 @@ except:
 
 if __name__ == "__main__":
     run = True
-    num = 0
-
-    objectrealmm = 50.8
-    focallen = 3.04
+    focal_length = 1032.78
     while run:
         ret, frame = vidStream.read()
-        huesatval = hsv(frame)
-        lower = np.array([110, 50, 50])
-        upper = np.array([130, 255, 255])
-        mask = cv2.inRange(huesatval, lower, upper)
-        cv2.imshow("mask",mask)
-        dilation = cv2.dilate(mask, np.ones((5, 5), np.uint8), iterations=6)
-        erode = cv2.erode(dilation, np.ones((5, 5), np.uint8), iterations=6)
-        im2, contours, hierarchy = cv2.findContours(erode, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        newcontours = []
-        for cnt in contours:
-            area = cv2.contourArea(cnt)
-            print(area)
-            if area > 2:
-                newcontours.append(cnt)
-        num_contours = len(newcontours)
-        print("num contours: ",num_contours)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        cv2.imshow("hsv",hsv)
+        edges = cv2.Canny(hsv, 100, 200)
+        cv2.imshow("edges",edges)
+
+
+        # huesatval = hsv(frame)
+        # lower = np.array([110, 50, 50])
+        # upper = np.array([130, 255, 255])
+        # mask = cv2.inRange(huesatval, lower, upper)
+        # cv2.imshow("mask",mask)
+        # dilation = cv2.dilate(mask, np.ones((5, 5), np.uint8), iterations=6)
+        # erode = cv2.erode(dilation, np.ones((5, 5), np.uint8), iterations=6)
+        # im2, contours, hierarchy = cv2.findContours(erode, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        # newcontours = []
+        # for cnt in contours:
+        #     area = cv2.contourArea(cnt)
+        #     print(area)
+        #     if area > 2:
+        #         newcontours.append(cnt)
+        # num_contours = len(newcontours)
+        # print("num contours: ",num_contours)
 
         #center,radius = cv2.minEnclosingCircle(newcontours[0])
 
@@ -56,10 +60,10 @@ if __name__ == "__main__":
 
        # distance_mm = objectrealmm * focallen / objsize
 
-
-        cv2.imshow("edited",erode)
-        #cv2.putText(frame,str(center),(frame.shape[1] - 800, frame.shape[0] - 100), cv2.FONT_HERSHEY_SIMPLEX,2.0, (0, 255, 0), 3)
-        cv2.imshow("frame",frame)
+        #
+        # cv2.imshow("edited",erode)
+        # #cv2.putText(frame,str(center),(frame.shape[1] - 800, frame.shape[0] - 100), cv2.FONT_HERSHEY_SIMPLEX,2.0, (0, 255, 0), 3)
+        # cv2.imshow("frame",frame)
         key = cv2.waitKey(1) & 0xFF
         if key == ord("q"):
             break
