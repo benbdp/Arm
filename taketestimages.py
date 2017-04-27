12f5bda8735325c6d5dc5e0abb41879208b477f4
Benjamin Plamondon <benjamin.plamondon@gmail.com>
2017-04-26 23:07:54 -0500

    take many images on pi

---
+++ taketestimages.py
@@ -0,0 +1,30 @@
+# import the necessary packages
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
+import cv2
+import numpy as np
+
+
+# initialize the camera and grab a reference to the raw camera capture
+camera = PiCamera()
+rawCapture = PiRGBArray(camera)
+
+camera.rotation = 180
+
+# allow the camera to warmup
+time.sleep(0.1)
+
+# grab an image from the camera
+
+def get_image():
+    camera.capture(rawCapture, format="bgr")
+    image = rawCapture.array
+    return image
+
+num = 100
+pic_num = 0
+
+for i in num:
+    cv2.imwrite("/home/pi/apples/"+str(pic_num)+".jpg",get_image())
+    time.sleep(0.1)
