d3dec375fbc93dda3345184d44f3f1938af5f90b
Benjamin Plamondon <benjamin.plamondon@gmail.com>
2017-05-15 00:10:55 -0400

    testing example code

--- Kinect/sync.py
+++ Kinect/sync.py
@@ -4,22 +4,30 @@ import cv2
 import frame_convert2
 
 cv2.namedWindow('Depth')
-cv2.namedWindow('Video')
+cv2.namedWindow('RGB')
+keep_running = True
+
+
+def display_depth(dev, data, timestamp):
+    global keep_running
+    cv2.imshow('Depth', frame_convert2.pretty_depth_cv(data))
+    if cv2.waitKey(10) == 27:
+        keep_running = False
+
+
+def display_rgb(dev, data, timestamp):
+    global keep_running
+    cv2.imshow('RGB', frame_convert2.video_cv(data))
+    if cv2.waitKey(10) == 27:
+        keep_running = False
+
+
+def body(*args):
+    if not keep_running:
+        raise freenect.Kill
+
+
 print('Press ESC in window to stop')
-
-
-def get_depth():
-    return frame_convert2.pretty_depth_cv(freenect.sync_get_depth()[0])
-
-
-def get_video():
-    return frame_convert2.video_cv(freenect.sync_get_video()[0])
-
-
-while 1:
-    cv2.imshow('Depth', get_depth())
-    cv2.imshow('Video', get_video())
-    k = cv2.waitKey(5) & 0xFF
-    if k == 27:
-        break
-cv2.destroyAllWindows()
+freenect.runloop(depth=display_depth,
+                 video=display_rgb,
+                 body=body)
