87a943e7df673223eacf3327109c6f5cb1d65915
benbdp <benjamin.plamondon@gmail.com>
2017-05-15 22:43:59 -0400

    using hough circle to identify apple

--- Kinect/find_circles.py
+++ Kinect/find_circles.py
@@ -19,11 +19,11 @@ while True:
     circles = np.uint16(np.around(circles))
     for i in circles[0,:]:
         # draw the outer circle
-        cv2.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
+        cv2.circle(frame,(i[0],i[1]),i[2],(0,255,0),2)
         # draw the center of the circle
-        cv2.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
+        cv2.circle(frame,(i[0],i[1]),2,(0,0,255),3)
 
-    cv2.imshow('detected circles',cimg)
+    cv2.imshow('detected circles',frame)
     k = cv2.waitKey(5) & 0xFF
     if k == 27:
         break
