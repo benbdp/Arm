f6af8ac369d42ad07c1837575cf61ce91224025c
benbdp <benjamin.plamondon@gmail.com>
2017-05-15 23:05:40 -0400

    testing canny

--- Kinect/find_circles.py
+++ Kinect/find_circles.py
@@ -17,23 +17,25 @@ while True:
     frame = cv2.blur(frame, (5, 5),5)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     edges = cv2.Canny(frame, 100, 200)
-    cv2.imshow("edges",edges)
-    #
-    # # detect circles in the image
-    # circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1.2, 100)
-    #
-    # # ensure at least some circles were found
-    # if circles is not None:
-    #     # convert the (x, y) coordinates and radius of the circles to integers
-    #     circles = np.round(circles[0, :]).astype("int")
-    #
-    #     # loop over the (x, y) coordinates and radius of the circles
-    #     for (x, y, r) in circles:
-    #         # draw the circle in the output image, then draw a rectangle
-    #         # corresponding to the center of the circle
-    #         cv2.circle(output, (x, y), r, (0, 255, 0), 4)
-    #         cv2.rectangle(output, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
-    # cv2.imshow("output", output)
+
+
+    # detect circles in the image
+    circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1.2, 100)
+
+    # ensure at least some circles were found
+    if circles is not None:
+        # convert the (x, y) coordinates and radius of the circles to integers
+        circles = np.round(circles[0, :]).astype("int")
+
+        # loop over the (x, y) coordinates and radius of the circles
+        for (x, y, r) in circles:
+            # draw the circle in the output image, then draw a rectangle
+            # corresponding to the center of the circle
+            cv2.circle(edges, (x, y), r, (0, 255, 0), 4)
+            cv2.rectangle(edges, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
+
+
+    cv2.imshow("edges", edges)
     k = cv2.waitKey(5) & 0xFF
     if k == 27:
         break
