46bb018c6bffbaca320432289565793951434e4b
benbdp <benjamin.plamondon@gmail.com>
2017-05-15 22:42:25 -0400

    using hough circle to identify apple

--- Kinect/find_circles.py
+++ Kinect/find_circles.py
@@ -2,17 +2,19 @@ import cv2
 import numpy as np
 import freenect
 
+""""http://docs.opencv.org/3.0-beta/doc/py_tutorials/py_imgproc/py_houghcircles/py_houghcircles.html"""
+
 def get_video():
     array, _ = freenect.sync_get_video()
     array = cv2.cvtColor(array, cv2.COLOR_RGB2BGR)
     return array
 
 while True:
-    img = get_video()
-    img = cv2.medianBlur(img,5)
-    cimg = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
+    frame = get_video()
+    frame = cv2.medianBlur(frame,5)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    circles = cv2.HoughCircles(img,cv2.HOUGH_GRADIENT,1,20,param1=50,param2=30,minRadius=0,maxRadius=0)
+    circles = cv2.HoughCircles(gray,cv2.HOUGH_GRADIENT,1,20,param1=50,param2=30,minRadius=0,maxRadius=0)
 
     circles = np.uint16(np.around(circles))
     for i in circles[0,:]:
