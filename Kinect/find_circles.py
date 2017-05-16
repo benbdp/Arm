47b850f859547b78c87dc4bcea1ef6cafbd215a4
benbdp <benjamin.plamondon@gmail.com>
2017-05-15 23:12:22 -0400

    testing canny

--- Kinect/find_circles.py
+++ Kinect/find_circles.py
@@ -22,7 +22,7 @@ while True:
 
     # detect circles in the image
     circles = cv2.HoughCircles(edges, cv2.HOUGH_GRADIENT, 1, 100,
-                               param1=50, param2=30, minRadius=20, maxRadius=150)
+                               param1=50, param2=30, minRadius=20, maxRadius=80)
     # ensure at least some circles were found
     if circles is not None:
         # convert the (x, y) coordinates and radius of the circles to integers
