87f4d7689b58db3678b9c312b3f202d74eb21f81
Benjamin Plamondon <benjamin.plamondon@gmail.com>
2017-05-18 00:18:57 -0400

    testing magnet

--- ArmCode/magnet.py
+++ ArmCode/magnet.py
@@ -1,25 +1,29 @@
 import wiringpi
 
-# If using BCM GPIO numbers
-wiringpi.wiringPiSetupGpio()
-wiringpi.pinMode(18, 2)  # pwm only works on GPIO port 18
-wiringpi.pwmWrite(18, 0)  # duty cycle between 0 and 1024. 0 = off, 1024 = fully on
-
-try:
-    while True:
-        wiringpi.pwmWrite(18, 1024)  # duty cycle between 0 and 1024. 0 = off, 1024 = fully on
-
-except:
+def init_pwm():
+    wiringpi.wiringPiSetupGpio()
+    wiringpi.pinMode(18, 2)  # pwm only works on GPIO port 18
     wiringpi.pwmWrite(18, 0)  # duty cycle between 0 and 1024. 0 = off, 1024 = fully on
 
+def mag_on():
+    wiringpi.pwmWrite(18, 1024)
 
-#
-# # OR, using wiringpi numbers
-# wiringpi.wiringPiSetup()
-# wiringpi.pinMode(1, 2)  # pwm only works on wiringpi pin 1
-# wiringpi.pwmWrite(1, 0)  # duty cycle between 0 and 1024. 0 = off, 1024 = fully on
-#
-# # OR, using P1 header pin numbers
-# wiringpi.wiringPiSetupPhys()
-# wiringpi.pinMode(12, 2)  # pwm only works on P1 header pin 12
-# wiringpi.pwmWrite(12, 0)  # duty cycle between 0 and 1024. 0 = off, 1024 = fully on
+
+def mag_off():
+    wiringpi.pwmWrite(18, 0)
+
+
+if __name__ == "__main__":
+    try:
+        init_pwm()
+        on = True
+        while on:
+            entry = input("enter desired action: ")
+            if entry == "on":
+                mag_on()
+            elif entry == "off":
+                mag_off()
+            else:
+                on = False
+    except:
+        mag_off()
