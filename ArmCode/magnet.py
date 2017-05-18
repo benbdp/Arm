eaaeddce35cdd4932a0db3325cb63644ce22739b
Benjamin Plamondon <benjamin.plamondon@gmail.com>
2017-05-18 00:11:44 -0400

    testing magnet

--- ArmCode/magnet.py
+++ ArmCode/magnet.py
@@ -1,23 +1,17 @@
-#!/usr/bin/python
-
-"""https://www.raspberrypi.org/forums/viewtopic.php?f=44&t=31714"""
-
-import wiringpi
-
-LED  = 1 # gpio pin 12 = wiringpi no. 1 (BCM 18)
-
-# Initialize PWM output for LED
-wiringpi.wiringPiSetup()
-wiringpi.pinMode(LED, 2)     # PWM mode
 wiringpi.pwmWrite(LED, 0)    # OFF
+import wiringpi2 as wiringpi
 
-# # Set LED brightness
-# def led(led_value):
-#     wiringpi.pwmWrite(LED,led_value)
-#
-# led(1)
-try:
-    while True:
-        wiringpi.pwmWrite(LED, 1)
-except:
-    wiringpi.pwmWrite(LED, 0)  # OFF
+# If using BCM GPIO numbers
+wiringpi.wiringPiSetupGpio()
+wiringpi.pinMode(18, 2)  # pwm only works on GPIO port 18
+wiringpi.pwmWrite(18, 0)  # duty cycle between 0 and 1024. 0 = off, 1024 = fully on
+
+# OR, using wiringpi numbers
+wiringpi.wiringPiSetup()
+wiringpi.pinMode(1, 2)  # pwm only works on wiringpi pin 1
+wiringpi.pwmWrite(1, 0)  # duty cycle between 0 and 1024. 0 = off, 1024 = fully on
+
+# OR, using P1 header pin numbers
+wiringpi.wiringPiSetupPhys()
+wiringpi.pinMode(12, 2)  # pwm only works on P1 header pin 12
+wiringpi.pwmWrite(12, 0)  # duty cycle between 0 and 1024. 0 = off, 1024 = fully on
