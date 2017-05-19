b43182e95229d6d4177739efd110166e18eda4a8
root <ben11.bdp@gmail.com>
2017-05-19 18:03:31 -0400

    tune threshold

--- cascade/get_pos.py
+++ cascade/get_pos.py
@@ -1,44 +1,59 @@
+# import the necessary modules
+import freenect
 import cv2
 import numpy as np
-import time
-import freenect
 
-def fix_pos(frame,path):
-        num = 0
-        while True:
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            ret, dst = cv2.threshold(gray, 200, 255, cv2.THRESH_BINARY_INV)
-            dilation = cv2.dilate(dst, np.ones((5, 5), np.uint8), 1)
-            im, contours, hierarchy = cv2.findContours(dilation, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-            newcontours = []
-            for i in contours:
-                area = cv2.contourArea(i)
-                if area > 100:  # run test to ensure small contours are eliminated
-                    newcontours.append(i)
-            x, y, w, h = cv2.boundingRect(newcontours[0])
-            print(x,y,w,h)
-            crop_img = frame[y: y + h, x: x + w]
-            h, w = crop_img.shape[:2]
-            resized_image = cv2.resize(crop_img, (w, h))
-            cv2.imwrite(path + "tom" + str(num) + ".jpg", resized_image)
-            num += 1
-            time.sleep(1)
 
+stored_lower = np.load("/home/ubuntu/Arm/Kinect/stored_lower.npy")
+
+# function to get RGB image from kinect
 def get_video():
     array, _ = freenect.sync_get_video()
     array = cv2.cvtColor(array, cv2.COLOR_RGB2BGR)
     return array
 
-path = "/home/ubuntu/Arm/cascade/pics"
-# frame = get_video()
-# tom_pos(frame,path)
-while True:
-    frame = get_video()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    ret, dst = cv2.threshold(gray, 100, 255, cv2.THRESH_BINARY_INV)
-    dilation = cv2.dilate(dst, np.ones((5, 5), np.uint8), 1)
-    cv2.imshow("window",dilation)
-    k = cv2.waitKey(5) & 0xFF
-    if k == 27:
-        break
-cv2.destroyAllWindows()
+
+def find_apple(rgb,depth,lower):
+    try:
+        hsv = cv2.cvtColor(rgb,cv2.COLOR_BGR2HSV)
+        upper = np.array([180,255,255])
+        mask = cv2.inRange(hsv,lower,upper)
+        dilation = cv2.dilate(mask, np.ones((5, 5), np.uint8), iterations=2)  # dilate pixels to fill in gaps
+        erode = cv2.erode(dilation, np.ones((5, 5), np.uint8), iterations=1)  # cut away border pixels to reduce size
+        im2, contours, hierarchy = cv2.findContours(erode, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)  # contour detection
+        newcontours = []
+        for cnt in contours:
+            area = cv2.contourArea(cnt)
+            # print "area", area
+            if area > 400:  # run test to ensure small contours are eliminated
+                newcontours.append(cnt)
+
+        M = cv2.moments(newcontours[0])
+        cx = int(M['m10'] / M['m00'])
+        cy = int(M['m01'] / M['m00'])
+
+        cv2.circle(rgb,(cx,cy),10,(0,0,255),3)
+    except:
+        pass
+
+
+
+if __name__ == "__main__":
+    while True:
+        # get a frame from RGB camera
+        frame = get_video()
+        cv2.imshow('window', frame)
+
+
+        #find and draw apple
+        find_apple(frame,stored_lower)
+
+
+        # display RGB image
+        cv2.imshow('window with circles', frame)
+
+        # quit program when 'esc' key is pressed
+        k = cv2.waitKey(5) & 0xFF
+        if k == 27:
+            break
+    cv2.destroyAllWindows()
