697f3bd3fcffdb90a445e44dd184254541f9712a
Benjamin Plamondon <benjamin.plamondon@gmail.com>
2017-05-19 10:53:22 -0400

    New program to take imgs with either rgb or ir camera

--- Kinect/get_cal_imgs.py
+++ Kinect/get_cal_imgs.py
@@ -1,7 +1,14 @@
 import freenect
 import numpy as np
 import cv2
+import argparse
+"""http://stackoverflow.com/questions/7427101/dead-simple-argparse-example-wanted-1-argument-3-results"""
 
+parser = argparse.ArgumentParser(description="Program to take calibration images for Kinect")
+
+parser.add_argument('camera', help='Enter rgb or ir', nargs='?', default="check_string_for_empty")
+
+args = parser.parse_args()
 
 def get_ir_video():
     array, _ = freenect.sync_get_video(0, freenect.VIDEO_IR_10BIT)
@@ -19,45 +26,45 @@ def pretty_depth(depth):
     depth=depth.astype(np.uint8)
     return depth
 
+if args.camera == 'check_string_for_empty':
+    print ('Enter rgb or ir at the end ')
 
-path_ir = "/home/ubuntu/Arm/Kinect/cal_imgs/ir/"
-path_rgb = "/home/ubuntu/Arm/Kinect/cal_imgs/rgb/"
-num = 0
-while True:
-    #
-    rgb = get_rgb_video()
-    # cv2.imwrite(path_rgb + "rgb" + str(num) + ".jpg", rgb)
-    cv2.imshow("rgb", rgb)
+elif args.camera == "rgb":
+    print("You asked for rgb calibration")
+    path_rgb = "/home/ubuntu/Arm/Kinect/cal_imgs/rgb/"
+    num = 0
+    while True:
+        rgb = get_rgb_video()
+        cv2.imwrite(path_rgb + "rgb" + str(num) + ".jpg", rgb)
+        cv2.imshow("rgb", rgb)
 
-    ir = get_ir_video()
-    ir = pretty_depth(ir)
-    # cv2.imwrite(path_ir + "ir" + str(num) + ".jpg", ir)
-    cv2.imshow("ir", ir)
+        num += 1
+        k = cv2.waitKey(5) & 0xFF
+        if k == 27:
+            break
 
-    num += 1
-    k = cv2.waitKey(5) & 0xFF
-    if k == 27:
-        break
+    cv2.destroyAllWindows()
 
-cv2.destroyAllWindows()
+elif args.camera == "ir":
+    print("You asked for ir calibration")
+    path_ir = "/home/ubuntu/Arm/Kinect/cal_imgs/ir/"
+    num = 0
+    while True:
+
+        ir = get_ir_video()
+        ir = pretty_depth(ir)
+        cv2.imwrite(path_ir + "ir" + str(num) + ".jpg", ir)
+        cv2.imshow("ir", ir)
+
+        num += 1
+        k = cv2.waitKey(5) & 0xFF
+        if k == 27:
+            break
+
+    cv2.destroyAllWindows()
+
+else:
+    print("You made invalid entry")
 
 
-#
-# def pretty_depth(depth):
-#     np.clip(depth, 0, 2**10-1, depth)
-#     depth >>=2
-#     depth=depth.astype(np.uint8)
-#     return depth
-# if __name__ == "__main__":
-#     while 1:
-#         #get a frame from RGB camera
-#         frame = get_video()
-#         #display IR image
-#         frame = pretty_depth(frame)
-#         cv2.imshow('IR image',frame)
-#
-#         # quit program when 'esc' key is pressed
-#         k = cv2.waitKey(5) & 0xFF
-#         if k == 27:
-#             break
-#     cv2.destroyAllWindows()
+
