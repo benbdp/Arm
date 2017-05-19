cb6263a0d24f0e90ad2e45720a5a2bbd916dfc19
benbdp <benjamin.plamondon@gmail.com>
2017-05-18 22:02:34 -0400

    getting ir image

--- Kinect/get_cal_imgs.py
+++ Kinect/get_cal_imgs.py
@@ -10,21 +10,36 @@ def get_ir_video():
 
 def get_rgb_video():
     array, _ = freenect.sync_get_video()
+    array = cv2.cvtColor(array, cv2.COLOR_RGB2BGR)
     return array
+
+def get_video():
+    array,_ = freenect.sync_get_video(0,freenect.VIDEO_IR_10BIT)
+    return array
+def pretty_depth(depth):
+    np.clip(depth, 0, 2**10-1, depth)
+    depth >>=2
+    depth=depth.astype(np.uint8)
+    return depth
 
 
 path_ir = "/home/ubuntu/Arm/Kinect/cal_img/ir"
 path_rgb = "/home/ubuntu/Arm/Kinect/cal_imgs/rgb"
 num = 0
 while num < 30:
-
-    rgb = get_rgb_video()
-    cv2.imwrite(path_rgb + "rgb" + str(num) + ".jpg", rgb)
-    cv2.imshow("rgb", rgb)
-
-    ir = get_ir_video()
-    cv2.imwrite(path_ir + "ir" + str(num) + ".jpg", rgb)
-    cv2.imshow("ir", ir)
+    #
+    # rgb = get_rgb_video()
+    # cv2.imwrite(path_rgb + "rgb" + str(num) + ".jpg", rgb)
+    # cv2.imshow("rgb", rgb)
+    #
+    # ir = get_ir_video()
+    # cv2.imwrite(path_ir + "ir" + str(num) + ".jpg", rgb)
+    # cv2.imshow("ir", ir)
+    # get a frame from RGB camera
+    frame = get_video()
+    # display IR image
+    frame = pretty_depth(frame)
+    cv2.imshow('IR image', frame)
 
     num += 1
     k = cv2.waitKey(200) & 0xFF
