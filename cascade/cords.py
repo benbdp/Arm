be7dc607453f5310405899161bfca73d0e58293c
root <ben11.bdp@gmail.com>
2017-05-20 10:09:19 -0400

    save new image and write info

--- cascade/cords.py
+++ cascade/cords.py
@@ -48,9 +48,9 @@ if __name__ == "__main__":
                 cv2.imshow("img",img)
                 #find and draw apple
                 apple,x,y,w,h = find_apple(img,stored_lower,file)
-                img = cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
+                gray = cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
                 row,clo,cha=img.shape
-                cv2.imwrite(path_new + str(num) + ".jpg", img)
+                cv2.imwrite(path_new + str(num) + ".jpg", gray)
                 file.write(str(num) + ".jpg" +" " +str(cha) + " " + str(x) + ' ' + str(y) + ' ' + str(w) + ' ' + str(h) + "\r\n")
                 cv2.imshow('window', apple)
                 num=num +1
