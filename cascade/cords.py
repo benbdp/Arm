ba59c176a0bf6746cd2a33f05138a518f7edb93e
root <ben11.bdp@gmail.com>
2017-05-19 22:38:46 -0400

    cords

---
+++ cascade/cords.py
@@ -0,0 +1,52 @@
+import cv2
+import numpy as np
+import time
+import glob
+import os
+
+
+stored_lower = np.load("/home/ubuntu/Arm/Kinect/stored_lower.npy")
+
+def find_apple(rgb,lower):
+    try:
+        blur = cv2.blur(rgb, (5, 5))
+        hsv = cv2.cvtColor(blur,cv2.COLOR_BGR2HSV)
+        upper = np.array([180,255,255])
+        mask = cv2.inRange(hsv,lower,upper)
+        dilation = cv2.dilate(mask, np.ones((5, 5), np.uint8), iterations=2)  # dilate pixels to fill in gaps
+        erode = cv2.erode(dilation, np.ones((5, 5), np.uint8), iterations=1)  # cut away border pixels to reduce size
+        im2, contours, hierarchy = cv2.findContours(erode, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)  # contour detection
+        newcontours = []
+        for cnt in contours:
+            area = cv2.contourArea(cnt)
+            # print "area", area
+            if area > 400:  # run test to ensure small contours are eliminated
+                newcontours.append(cnt)
+        x, y, w, h = cv2.boundingRect(newcontours[0])
+        crop_img = rgb[y-2: y + h+2, x-2: x + w+2]
+        # h, w = crop_img.shape[:2]            #w    h
+        # resized_image = cv2.resize(crop_img, (48, 50))
+
+        return crop_img
+    except:
+        pass
+
+if __name__ == "__main__":
+    num = 520
+    path = "/home/ubuntu/Arm/cascade/pos/"
+    while True:
+        frame = glob.glob(os.path.join(path, '*.jpg'))
+        cv2.imshow('window', frame)
+
+
+        #find and draw apple
+        apple = find_apple(frame,stored_lower)
+
+        cv2.imshow('window', apple)
+        num = num +1
+
+        # quit program when 'esc' key is pressed
+        k = cv2.waitKey(1000) & 0xFF
+        if k == 27:
+            break
+    cv2.destroyAllWindows()
