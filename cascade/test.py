a4f2e9bdf810c23bf45bb698800b00b502c6f883
root <ben11.bdp@gmail.com>
2017-05-21 00:32:17 -0400

    added cascade

---
+++ cascade/test.py
@@ -0,0 +1,30 @@
+import numpy as np
+import cv2
+import freenect
+
+# this is the cascade we just made. Call what you want
+cascade = cv2.CascadeClassifier('/home/benjamin/PycharmProjects/Arm/cascade/data/cascade.xml')
+
+def get_video():
+    array, _ = freenect.sync_get_video()
+    array = cv2.cvtColor(array, cv2.COLOR_RGB2BGR)
+    return array
+
+while 1:
+    img = get_video()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # add this
+    # image, reject levels level weights.
+    apple = cascade.detectMultiScale(gray, 50, 50)
+
+    # add this
+    for (x, y, w, h) in apple:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 255, 0), 2)
+    cv2.imshow('img', img)
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
