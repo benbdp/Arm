555abb109a8cc948da45df8b3d8a1f3aca909b83
root <ben11.bdp@gmail.com>
2017-05-21 00:35:39 -0400

    added cascade

--- cascade/test.py
+++ cascade/test.py
@@ -16,15 +16,15 @@ while 1:
 
     # add this
     # image, reject levels level weights.
-    apple = cascade.detectMultiScale(gray, 50, 50)
+    apple = cascade.detectMultiScale(gray, 20, 20)
 
     # add this
     for (x, y, w, h) in apple:
         cv2.rectangle(img, (x, y), (x + w, y + h), (255, 255, 0), 2)
+
     cv2.imshow('img', img)
     k = cv2.waitKey(30) & 0xff
     if k == 27:
         break
 
-cap.release()
 cv2.destroyAllWindows()
